7e1af9288464cfebe4c8e2aae9e4957b7b359546
dancadev <dancadev@gmail.com>
2023-03-10 18:44:45 -0500

    Hola.js

---
+++ js-1/hello.js
@@ -0,0 +1,9 @@
+function hola(t){
+    console.log("Hola " +t + "!" )
+    
+}
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
