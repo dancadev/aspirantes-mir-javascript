bc80d442a072f0302fb5f13bd9f70e9d9ed2750c
dancadev <dancadev@gmail.com>
2023-03-15 12:31:36 -0500

    Funcion suma

---
+++ js-2/sum.js
@@ -0,0 +1,12 @@
+function sum (arreglo){
+    let respuesta = 0
+    for(let i = 0;i < arreglo.length ;i+=1){
+        respuesta += arreglo[i]
+    }
+    return respuesta
+}
+// escribe la función sum acá
+
+console.log(sum([1, 2, 3])) // 6
+console.log(sum([10, 8, 12, 5])) // 35
+console.log(sum([])) // 0
